844c52cac3b5dfa1813a0ff5e4bbb3ddf6152690
Shokhrukh <shokhrukh@admins-MBP.attlocal.net>
2024-02-12 20:05:17 -0600

    modified ALB

--- ALB.tf
+++ ALB.tf
@@ -1,32 +1,34 @@
+# Create ALB in the project
 resource "aws_alb" "projectALB" {
-    name = "Project ALB"
-    subnets = aws_subnet.public.*.id
-    security_groups = [aws_security_group.ALB.id]  
+  name = "projectAlB"
+  subnets = aws_subnet.public.*.id
+  security_groups = [aws_security_group.ALB.id]
 }
 resource "aws_alb_target_group" "projectTG" {
-    name = "projectTG"
-    port = 80
+  name = "projectTG"
+  port = 80
+  protocol = "HTTP"
+  vpc_id = aws_vpc.vpcForProject.id
+  target_type = "ip"
+
+  health_check {
+    healthy_threshold = "3"
+    interval = "30"
     protocol = "HTTP"
-    vpc_id = aws_vpc.vpcForProject.id
-    target_type = "ip"
-    health_check {
-      healthy_threshold = "3"
-      interval = "30"
-      protocol = "HTTP"
-      matcher = "200"
-      timeout = "3"
-      path = var.health_check_path
-      unhealthy_threshold = "2"
-    }
+    matcher = "200"
+    timeout = "3"
+    path = var.health_check_path
+    unhealthy_threshold = "2"
+  }
 }
 
 resource "aws_alb_listener" "projectListener" {
-    load_balancer_arn = aws_alb.projectALB.arn
-    port = var.app_port
-    protocol = "HTTP" 
+  load_balancer_arn = aws_alb.projectALB.arn
+  port              = var.app_port
+  protocol          = "HTTP"
 
-    default_action {
-     target_group_arn = aws_alb_target_group.projectTG.arn
-     type = "forward" 
-    } 
+  default_action {
+    target_group_arn = aws_alb_target_group.projectTG.arn  # Specify the correct target group ARN here
+    type             = "forward"
+  }
 }
